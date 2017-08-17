a7be017f03245ee4b944d8412516a60d9ab9a6f7
DESKTOP-J2BSH1E\Marishka <marishka.zachariah@globacore.com>
2017-08-17 16:10:33 -0400

    implementing assets, started timer script

---
+++ NebulaDerbyUnity/Assets/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    //coroutine start off 2 min 
+    public float timeLeft = 30.0f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            GameOver();
+        }
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	public void GameOver()
+    {
+        Debug.Log("GameOver");
+    }
+
+
+}
